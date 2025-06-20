da419184c0543890b390ecd584a1c887f8c64dda
polarhexagon <maxresdefault111@gmail.com>
2025-06-20 14:20:13 -0700

    planet generation is go!

--- main.lua
+++ main.lua
@@ -1,17 +1,52 @@
 function love.load()
   Object = require "classic"
+  push = require "push"
+  require "planet"
+  
+  -- push
+  WINDOW_WIDTH   = 960
+  WINDOW_HEIGHT  = 720
+  VIRTUAL_WIDTH  = 320
+  VIRTUAL_HEIGHT = 240
+  love.graphics.setDefaultFilter("nearest","nearest")
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+  
   planet = Planet()
-  math.randomseed(os.time)
+  output = io.output("out.txt")
+  
+  hitbox = {}
+  hitbox.width = 60
+  hitbox.height = 20
+  hitbox.x = VIRTUAL_HEIGHT - hitbox.height - 20
+  hitbox.y = VIRTUAL_WIDTH - hitbox.width
+  
+  math.randomseed(os.time())
+  
+  smallFont = love.graphics.newFont("DepartureMono-Regular.otf", 11)
+  love.graphics.setFont(smallFont)
 end
 
 function love.keypressed(key)
-  
+  if key == "space" then
+    planet:generate()
+  end
 end
 
 function love.update(dt)
-  
+  --planet:generate() -- stress testing
+  --output:write(planet.radius .. "\n")
 end
 
 function love.draw()
-  
+  push:apply("start")
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255) -- RGBA
+    love.graphics.setColor(1,0,0)
+    love.graphics.rectangle("fill",hitbox.x,hitbox.y,hitbox.width,hitbox.height)
+    planet:draw()
+    --love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS( )), 10, VIRTUAL_HEIGHT-20)
+  push:apply("end")
 end
--- planet.lua
+++ planet.lua
@@ -1,19 +1,14 @@
 Planet = Object:extend()
 
-function Planet:new()
-  -- Blank values to be filled in later
-  self.temp = 0
-  self.radius = 0
-  self.mass = 0
-  self.gravity = 0
-  self.moons = 0
-  self.rings = 0
-end
+local newton_constant = 6.6743e-11
 
+--[[
+  Helper functions for random values and crunching numbers.
+]]
 -- roll numDice amount of random numbers in given bounds, return the lowest rolled number
-function countLowest(numDice, lowerBound, upperBound)
-  local lowestRoll = lowerBound
-  for i = 1,numDice do
+local function countLowest(numDice, lowerBound, upperBound)
+  local lowestRoll = upperBound
+  for i = 0,numDice do
     local currentRoll = math.random(lowerBound, upperBound)
     if lowestRoll > currentRoll then
       lowestRoll = currentRoll
@@ -22,15 +17,66 @@ function countLowest(numDice, lowerBound, upperBound)
   return lowestRoll
 end
 
+-- generate a random normal within a log-normal dist
+local function logRand(stdDev, mean)
+  -- very rough approximate of a random Gaussian value
+  local std_normal = (math.random()*2-1 + math.random()*2-1) / 2
+  --print(std_normal) --debug
+  local normal_value = stdDev * std_normal + mean
+  return math.exp(normal_value)
+end
+
+-- calculate gravity of a given body from radius and mass
+local function gravity(radius, mass)
+  local earth_mass = 5.972168e24
+  local earth_radius = 6371
+  radius = radius * earth_radius
+  mass = mass * earth_mass
+  mass = ((newton_constant*(mass/(radius^2)))/1000000)*0.107
+  return mass
+end
+
+-- should round numbers, only use this for prints at the end
+local function round(value, decimals)
+  value = value * decimals
+  value = math.floor(value)
+  value = value/decimals
+  return value
+end
+
+--[[
+  Functions for planets.
+]]
+
+function Planet:new()
+  -- Blank values to be filled in later
+  self.temp = 0
+  self.radius = 0
+  self.mass = 0
+  self.gravity = 0
+  self.moons = 0
+end
+
+-- Replace old planet values with new ones. Cool
 function Planet:generate()
-  local minTemp = -275*10
-  local maxTemp = 500*10
-  -- Rolls twice and averages results
-  self.temp = math.random(min_temp, max_temp)+math.random(min_temp, max_temp)/20
   
-  -- Measured in Earth masses.
-  local minMass = 0.0041
-  local maxMass = 4134
+  -- Measured in Celsius.
+  local min_temp = -275*10
+  local max_temp = 500*10
+  -- Rolls three times and averages results
+  self.temp = (math.random(min_temp, max_temp)+
+               math.random(min_temp, max_temp)+
+               math.random(min_temp, max_temp))/30
+  
+  -- These measurements are all in comparison to Earth.
+  -- ie. Earth radii, Earth masses, Earth gravity...
+  self.radius = countLowest(2, 3, 50)/10
+  -- ideal range 0.0041 - 4134
+  -- current range 0.001 - 200, avg 0.1-10
+  self.mass = logRand(6, -0.5)
+  self.gravity = gravity(self.radius, self.mass)
+  
+  self.moons = countLowest(3,0,6)
   
 end
 
@@ -39,5 +85,10 @@ function Planet:update(dt)
 end
 
 function Planet:draw()
-  
+  love.graphics.setColor(0,1,0)
+  love.graphics.print("temp:    " .. tostring(round(self.temp, 10)) .. " C", 10, 10)
+  love.graphics.print("radius:  " .. tostring(self.radius, 10) .. " RE", 10, 20)
+  love.graphics.print("mass:    " .. tostring(round(self.mass, 1000)) .. " ME", 10, 30)
+  love.graphics.print("gravity: " .. tostring(round(self.gravity, 100)) .. " GE", 10, 40)
+  love.graphics.print("moons:   " .. tostring(self.moons), 10, 50)
 end
